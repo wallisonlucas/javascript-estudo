5029cdd97f94b9918f998bc89ba17cfbb3026ace
Wallison Lucas <araujollucas@gmail.com>
2023-10-07 16:24:13 -0300

    Cadastrar fornecedores

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,22 @@
 <template>
 
-  <div style="width: 50%; float: left;">
+  <div style="width: 25%; float: left;">
     <CadastrarCliente />
   </div>
 
-  
+  <div style="width: 25%; float: left;">
+    <ListarCliente />
+  </div>
+
+  <div style="width: 25%; float: left;">
+    <CadastrarFornecedor />
+  </div>
+
 </template>
 
 <script setup>
-import CadastrarCliente from "./components/clientes/CadastrarCliente.vue";
+import CadastrarCliente from "./components/clientes/CadastrarCliente.vue"
+import ListarCliente from "./components/clientes/ListarCliente.vue"
+import CadastrarFornecedor from "./components/fornecedores/CadastrarFornecedor.vue"
 </script>
 
---
+++ frontend/src/components/clientes/ListarCliente.vue
@@ -0,0 +1,38 @@
+<template>
+    <h3>Clientes Cadastrados</h3>
+    <ul>
+        <li v-for="cliente in clientes">{{ cliente.nome }}</li>
+    </ul>
+</template>
+
+
+<script>
+
+import axios from 'axios'
+
+const minhaApi = axios.create({
+    baseURL: "http://localhost:3000",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    name: "ListarCliente",
+    data(){
+        return {
+            clientes: []
+        }
+    },
+    async mounted(){
+        this.listar()
+    },
+    methods: {
+       async listar(){
+        const responce = await minhaApi.get("/cliente/")
+        this.clientes = responce.data
+        console.log(responce.data);
+       }
+    }
+}
+</script>
